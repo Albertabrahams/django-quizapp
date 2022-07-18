c506c221b21022654d26bb8e90a844b3ce1a9f7a
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-07-19 00:02:43 +0300

    serializers done

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Quiz
+from .serializers import CategorySerializer
 
-# Create your views here.
+class CategoryList(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetail(generics.ListAPIView):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        queryset = Quiz.objects.all()
+        category = self.kwargs['category']
+        return queryset.filter(category__name=category)
+        
